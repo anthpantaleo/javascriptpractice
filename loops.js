522a212d62ae51e8970a16297b2d8b892ba1bfa5
anthpantaleo <anthpantaleo@gmail.com>
2022-09-04 13:48:02 -0400

    Working with loops from MDN

---
+++ loops.js
@@ -0,0 +1,18 @@
+// const output = document.querySelector(".output");
+// output.innerHTML = "";
+
+// let i = 10;
+
+// while (i >= 0) {
+//   const para = document.createElement("p");
+//   if (i === 10) {
+//     para.textContent = `Coundown ${i}`;
+//   } else if (i < 10 && i > 0) {
+//     para.textContent = `${i}`;
+//   } else {
+//     para.textContent = `BLASTOFF!`;
+//   }
+
+//   output.appendChild(para);
+//   i--;
+// }
